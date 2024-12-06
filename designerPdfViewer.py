1645bac8f6e5c713cc33a45d9b6014434c8ac03c
Raisul Islam <mechrsl0@gmail.com>
2024-12-06 20:09:37 +0600

    Create designerPdfViewer.py

---
+++ designerPdfViewer.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'designerPdfViewer' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h
+#  2. STRING word
+#
+
+def designerPdfViewer(h, word):
+    # Write your code here
+    word = word.lower()
+    maxh = 0
+    
+    
+    for char in word:
+        if char.isalpha():
+            position = ord(char) - ord('a')
+            if maxh < h[position]:
+                maxh = h[position]
+            
+    return maxh*len(word)
+            
+    
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
